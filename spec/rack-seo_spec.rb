6738ff24a2f6536acc55acede777f13d320e00e8
Xavier Riley <xavier@kyanmedia.com>
2013-02-19 21:09:08 +0000

    Setup spec helpers and fixtures. Wrote first failing test around h1

--- spec/rack-seo_spec.rb
+++ spec/rack-seo_spec.rb
@@ -1,7 +1,24 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
-
 describe "RackSeo" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  before do
+    @rack_seo = RackSeo.new :public => Fixtures.path, :store => {}
+    @env = Rack::MockRequest.env_for '/'
+  end
+
+  context "sensible defaults" do
+    it "pulls the h1 tag into the meta title by default" do
+      orig_meta_title = @rack_seo.css('title')
+      orig_h1 = @rack_seo.css('h1').first
+      @rack_seo.execute!
+      new_meta_title = @rack_seo.css('title')
+      new_h1 = @rack_seo.css('h1').first
+      new_meta_title.should == orig_h1
+      new_meta_title.should_not ==  orig_h1
+    end
+
+    it "summarises the description based on the text in #content if it exists"
+    it "summarises the description based on the text in the page if #content does not exist"
+    it "summarises the keywords based on the text in #content if it exists"
+    it "summarises the keywords based on the text in the page if #content does not exist"
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,41 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
+require 'rack/pagespeed'
+require 'rack/pagespeed/store/disk'
+require 'fileutils'
+require 'tmpdir'
+require 'ostruct'
 require 'rack-seo'
+
+def fixture name
+  File.read(File.join(Fixtures.path, name))
+end
+
+module Apps
+  class << self
+    def complex
+      lambda { |env| [200, { 'Content-Type' => 'text/html' }, [fixture('complex.html')]] }
+    end
+
+    def plain_text
+      lambda { |env| [200, { 'Content-Type' => 'text/plain' }, ['plain texto']] }
+    end
+  end
+end
+
+Fixtures = OpenStruct.new unless defined?(Fixtures)
+Fixtures.path = File.join(File.dirname(__FILE__), 'fixtures')
+Fixtures.complex = Nokogiri::HTML(fixture('complex.html'))
+Fixtures.complex = Nokogiri::HTML(fixture('simple.html'))
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-  
+  config.before :each do
+    Fixtures.complex = Nokogiri::HTML(fixture('complex.html'))
+    Fixtures.simple = Nokogiri::HTML(fixture('simple.html'))
+  end
 end
